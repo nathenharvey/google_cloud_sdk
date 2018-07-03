56b97bb4c6f0545fe41059a310f8f811629f625f
Nathen Harvey <nharvey@chef.io>
2018-07-03 11:13:44 -0400

    Install on Ubuntu 16.04
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+cloud_sdk_repo = "cloud-sdk-#{node['lsb']['codename']}"
+
+apt_repository 'google-cloud-sdk' do
+  uri 'http://packages.cloud.google.com/apt'
+  distribution ''
+  components [cloud_sdk_repo, 'main']
+  keyserver 'https://packages.cloud.google.com'
+  key 'https://packages.cloud.google.com/apt/doc/apt-key.gpg'
+  cache_rebuild true
+end
+
+# for testing only
+package 'vim'
+
+package 'google-cloud-sdk'
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -18,18 +18,13 @@ describe 'google_cloud_sdk::default' do
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it 'creates an apt repository' do
+      expect(chef_run).to add_apt_repository('google-cloud-sdk')
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'installs the google-cloud-sdk package' do
+      expect(chef_run).to install_package('google-cloud-sdk')
     end
   end
 end
