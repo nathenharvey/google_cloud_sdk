e1034e2708bf741688f5c5c5150560fe3e119ea4
Nathen Harvey <nharvey@chef.io>
2018-07-03 11:31:01 -0400

    Add support for Ubuntu 14.04
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,24 +7,26 @@
 require 'spec_helper'
 
 describe 'google_cloud_sdk::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
-    end
+  %w( 16.04 14.04 ).each do |os_version|
+    context "When all attributes are default, on Ubuntu #{os_version}" do
+      let(:chef_run) do
+        # for a complete list of available platforms and versions see:
+        # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+        runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: os_version)
+        runner.converge(described_recipe)
+      end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+      it 'converges successfully' do
+        expect { chef_run }.to_not raise_error
+      end
 
-    it 'creates an apt repository' do
-      expect(chef_run).to add_apt_repository('google-cloud-sdk')
-    end
+      it 'creates an apt repository' do
+        expect(chef_run).to add_apt_repository('google-cloud-sdk')
+      end
 
-    it 'installs the google-cloud-sdk package' do
-      expect(chef_run).to install_package('google-cloud-sdk')
+      it 'installs the google-cloud-sdk package' do
+        expect(chef_run).to install_package('google-cloud-sdk')
+      end
     end
   end
 end
